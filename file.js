49443eabafd5f367158e2af8bd9d6cfdb6c7272f
Silviu Barbu <s.barbu@goit.global>
2025-04-15 21:18:53 +0300

    Lectia 1 NodeJS

---
+++ file.js
@@ -0,0 +1,10 @@
+// Adaugam in package.json -> "type": "module"  // pentru a folosi module CommonJS
+// Documentatie nodejs -> https://nodejs.org
+
+// fs.writeFile("mynewfile4.txt", "Hello content!", function (err) {
+//   if (err) throw err;
+//   console.log("Saved!");
+// });
+
+console.log("Dirname: ", __dirname);
+console.log("Filename: ", __filename);
